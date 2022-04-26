7da2f3033839b2e554c1b68e3d07eb4cb34cd4c3
keni <926191490@qq.com>
2022-04-27 00:51:38 +0800

    feat: uploadForm 页面version 1.0

--- src/pages/uploadProduct/UploadProduct.tsx
+++ src/pages/uploadProduct/UploadProduct.tsx
@@ -1,49 +1,46 @@
-import { Alert, Form, Input, InputNumber } from 'antd';
+import { Alert, Form, Input, InputNumber, message } from 'antd';
 import { useForm } from 'antd/lib/form/Form';
-import AmountInput from 'components/AmountInput/AmountInput';
 import NormalBreadcrumb from 'components/NormalBreadcrumb/NormalBreadcrumb';
+import NormalButton from 'components/NormalButton/NormalButton';
 import UploadImage from 'components/UploadImage/UploadImage';
 import React, { FC, useState } from 'react';
-import { formatNumber } from 'utils';
+import { useNavigate } from 'react-router-dom';
+import { formatNumber, parseFormatNumber } from 'utils';
 import './style.scss';
 
 const UploadProduct: FC<{}> = () => {
   const [uploadForm] = useForm();
-  const { validateFields, setFieldsValue, getFieldValue } = uploadForm;
-
-  const onValuesChange = (changedValues: any, allValues: any) => {
-    console.log(changedValues, allValues);
+  const { validateFields, setFieldsValue } = uploadForm;
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const onInputNumberChange = (e: unknown, fieldName: string) => {
+    const newValue = e ? parseFormatNumber(e as string) : '0';
+    setFieldsValue({
+      [fieldName]: formatNumber(newValue),
+    });
   };
 
-  // const onPriceChange = (e: any) => {
-  //   const value = getFieldValue('productPrice');
-  //   const priceValue = e.target.value;
-  //   console.log(value)
-  //   const reg = /^-?\d*(\.\d*)?$/;
-  //   if (
-  //     (!isNaN(priceValue) && reg.test(priceValue)) ||
-  //     priceValue === '' ||
-  //     priceValue === '-'
-  //   ) {
-  //     setFieldsValue({
-  //       productPrice: formatNumber(priceValue),
-  //     });
-  //   } else {
-  //     console.log('输入的不是数字');
-  //     console.log(e)
-  //     e.cancelable = true;
-  //     e.preventDefault();
-  //     e.stopPropagation();
-  //   }
-  // };
-  // const onPriceBlur = () => {
-  //   const value = getFieldValue('productPrice');
-  //   let valueTemp = value;
-  //   if (value.charAt(value.length - 1) === '.' || value === '-') {
-  //     valueTemp = value.slice(0, -1);
-  //   }
-  //   onPriceChange();
-  // };
+  const handleButtonClick = (e: any) => {
+    setLoading(true);
+    e.target.blur();
+    validateFields()
+      .then(values => {
+        console.log('数据处理');
+        console.log(values);
+        const timer = setTimeout(() => {
+          message.success('上传成功');
+          clearTimeout(timer);
+          setLoading(false);
+          navigate('/home');
+        }, 1000);
+      })
+      .catch(err => {
+        console.log(err);
+        message.error('上传失败');
+        setLoading(false);
+      });
+  };
+
   return (
     <>
       <NormalBreadcrumb categoryName={'上传商品'} />
@@ -54,40 +51,76 @@ const UploadProduct: FC<{}> = () => {
           showIcon
           closable
         />
-        <Form form={uploadForm} onValuesChange={onValuesChange}>
+        <Form form={uploadForm} layout="vertical">
           <div className="upload-form-title">商品信息</div>
           <div className="upload-form-content">
-            <Form.Item label="商品名称" name="productName">
-              <Input className="product-name" placeholder="请输入商品名称" />
-            </Form.Item>
-            <Form.Item label="商品价格" name="productPrice">
-              <Input
-                placeholder="请输入商品价格"
-                className="product-price"
-                // onBlur={onPriceBlur}
-              />
-            </Form.Item>
-            <Form.Item label="原价" name="productBasePrice">
-              <Input
-                placeholder="请输入商品价格"
-                className="product-price"
-                // onChange={onPriceChange}
-                // onBlur={onPriceBlur}
-              />
-            </Form.Item>
-            <Form.Item label="库存" name="productStock">
-              <InputNumber min={0} max={999} controls={false} />
-            </Form.Item>
-            <Form.Item label="商品图片" name="productImage">
-              <UploadImage />
-            </Form.Item>
-            <Form.Item label="商品描述" name="productDesc">
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item label="商品地址" name="productDetail">
-              <Input.TextArea />
-            </Form.Item>
+            <div className="image-upload">
+              <Form.Item label="商品图片" name="productImage">
+                <UploadImage />
+              </Form.Item>
+            </div>
+            <div className="info-upload">
+              <Form.Item label="商品名称" name="productName">
+                <Input
+                  className="upload-input product-name"
+                  placeholder="请输入商品名称"
+                />
+              </Form.Item>
+              <Form.Item label="商品价格" name="productPrice">
+                <InputNumber
+                  placeholder="请输入商品价格"
+                  className="upload-input product-input-number"
+                  onChange={e => onInputNumberChange(e, 'productPrice')}
+                  controls={false}
+                  min={0}
+                  max={99999}
+                />
+              </Form.Item>
+              <Form.Item label="原价" name="productBasePrice">
+                <InputNumber
+                  placeholder="请输入商品价格"
+                  className="upload-input product-input-number"
+                  onChange={e => onInputNumberChange(e, 'productBasePrice')}
+                  controls={false}
+                  min={0}
+                  max={99999}
+                />
+              </Form.Item>
+              <Form.Item label="库存" name="productStock">
+                <InputNumber
+                  placeholder="请输入库存数量"
+                  className="upload-input product-input-number"
+                  onChange={e => onInputNumberChange(e, 'productStock')}
+                  min={0}
+                  max={999}
+                  controls={false}
+                />
+              </Form.Item>
+              <Form.Item
+                className="upload-input"
+                label="商品描述"
+                name="productDesc"
+              >
+                <Input.TextArea />
+              </Form.Item>
+              <Form.Item
+                className="upload-input"
+                label="商品地址"
+                name="productDetail"
+              >
+                <Input.TextArea />
+              </Form.Item>
+            </div>
           </div>
+          <Form.Item>
+            <NormalButton
+              className="upload-button"
+              content="上传"
+              htmlType="submit"
+              loading={loading}
+              onClick={handleButtonClick}
+            />
+          </Form.Item>
         </Form>
       </div>
     </>
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,8 @@
 export function formatNumber(value: string): string {
+  return `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+export function parseFormatNumber(value: string): string {
   value += '';
-  const list = value.split('.');
-  const prefix = list[0].charAt(0) === '-' ? '-' : '';
-  let num = prefix ? list[0].slice(1) : list[0];
-  let result = '';
-  while (num.length > 3) {
-    result = `,${num.slice(-3)}${result}`;
-    num = num.slice(0, num.length - 3);
-  }
-  if (num) {
-    result = num + result;
-  }
-  return `${prefix}${result}${list[1] ? `.${list[1]}` : ''}`;
+  return value.replace(/\s?|(,*)/g, '');
 }
